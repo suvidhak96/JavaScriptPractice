4e0c932aa5472aead35fa7fdb39922fed8e39d4c
Suvidha Kamble <122528736+suvidhak96@users.noreply.github.com>
2024-03-16 16:24:51 +0530

    Add files via upload

---
+++ practice/DOM/index.js
@@ -0,0 +1,24 @@
+// let a=document.getElementById('head1')
+// let b=document.getElementsByClassName("class")
+// let c=document.getElementsByTagName("p")
+// console.log(c)
+// console.log(a)
+
+
+// let a= document.querySelector("div").textContent
+// console.dir(a)
+
+// let a= document.querySelector("h2");
+// a.innerText=a.innerText+" hjhhjhj"
+// console.log(a.innerText)
+
+// let divs=document.querySelectorAll(".box");
+// let i=1
+// for(let div of divs){
+// div.innerText=`update ${i}`
+// i++
+//     console.log(div.innerText)
+// }
+
+
+// clsure
